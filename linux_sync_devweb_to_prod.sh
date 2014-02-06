0b934989a0fa787de1d19b4a1f78d2b1d59c691d
isdvee <dveer@northwest.ca>
2014-02-06 11:28:13 -0600

    Changed the script to do an initial pull of northwest.ca content from prod to ghadminlx. Eventually the script will pull from devweb2 to ghadminlx and then push that to nwc-ux2 (prod).

--- linux_sync_devweb_to_prod.sh
+++ linux_sync_devweb_to_prod.sh
@@ -38,8 +38,8 @@ echo "Starting rsync process..." >> $RSYNC_OUTPUT 2>&1
 # Starting to run the rsync command. All output is captured into a file at the end.
 
 	# Copy the files 
-	echo /usr/bin/rsync -ruvap --progress isdvee@devweb2.northwest.ca:/srv/northwest.ca/ /srv/websites/northwest.ca-$(date +%d-%m-%Y)
-	/usr/bin/rsync -ruvap --progress isdvee@devweb2.northwest.ca:/srv/northwest.ca/ /srv/websites/northwest.ca-$(date +%d-%m-%Y) >> $RSYNC_OUTPUT 2>&1
+	echo /usr/bin/rsync -ruvap --progress djveer@198.1.62.19:/srv/northwest.ca/ /srv/websites/northwest.ca-$(date +%d-%m-%Y)
+	/usr/bin/rsync -ruvap --progress djveer@198.1.62.19:/srv/northwest.ca/ /srv/websites/northwest.ca-$(date +%d-%m-%Y) >> $RSYNC_OUTPUT 2>&1
 	rc=$?
 	echo "rsync returned $rc"
 	if [[ $rc != 0 ]] ; then
