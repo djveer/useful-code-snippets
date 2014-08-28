3e267c563b43affeb18ba57796c4dcee9f647ca6
isdvee <dveer@northwest.ca>
2014-08-28 14:32:24 -0500

    Updated file to include a search for ~/.ssh/authorized_keys files of larger than 0kb to narrow the search to active hubs.

--- linux_tnt_check_for_sftp_xml_files.sh
+++ linux_tnt_check_for_sftp_xml_files.sh
@@ -2,7 +2,7 @@
 # File for checking/notification of delayed transfer of OTM XML files to the TNT hubs.
 #
 
-SFTP_HOME=/home/isdvee/Test
+SFTP_HOME=/srv/home
 RUNDATE=`date +%d.%m.%Y-%H%M`
 NOTIFY=dveer@northwest.ca
 MAX_AGE_MIN=15
@@ -10,12 +10,13 @@ LOG=/tmp/check_for_sftp_files.$RUNDATE.log
 LOGGER="tee -a $LOG"
 HOSTNAME=$(hostname -s)
 
-# find all dirs under /srv/home with an authorized_keys file larger than 0k (not empty)
+# find all dirs under /srv/home with an authorized_keys file larger than 0k (not empty) to find the active hubs
+ACTIVE_HUBS=$(find $SFTP_HOME -name authorized_keys -type f -size +0k | cut -f1 -d".")
 
-# find XML files that have a date older than 15 minutes
+# find XML files that have a date older than 15 minutes for the active hubs
 echo ">>> Finding XML files older than $MAX_AGE_MIN minutes..." | $LOGGER
 echo -en '\n' | $LOGGER
-find $SFTP_HOME -name *.xml -type f -mmin +$MAX_AGE_MIN | $LOGGER
+find $ACTIVE_HUBS -name *.xml -type f -mmin +$MAX_AGE_MIN | $LOGGER
 
 # find the number of occurrences in the log file
 FILES=$(grep .xml $LOG | wc -l)
@@ -31,14 +32,14 @@ then
 	echo "This message has been auto-generated, do not reply to this email."
 	echo -en '\n' | $LOGGER
 	echo ">>> Alert generated - $RUNDATE on $(hostname)" | $LOGGER
-	cat $LOG | mailx -s "Delayed TNT SFTP XML files have been found on $HOSTNAME at $RUNDATE" -r noreply@northwest.ca $NOTIFY | $LOGGER
+	cat $LOG | echo mailx -s "Delayed TNT SFTP XML files have been found on $HOSTNAME at $RUNDATE" -r noreply@northwest.ca $NOTIFY | $LOGGER
 else
 	echo -en '\n' | $LOGGER
 	echo ">>> No delayed files have been found. Thus, exiting." | $LOGGER
 fi
 
 # Delete the output file, if file exists and size is greater than 0 KB.
-#if [ -s $LOG ]
-#then
-#        rm $LOG
-#fi
+if [ -s $LOG ]
+then
+        rm $LOG
+fi
