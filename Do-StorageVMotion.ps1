2a34b7f4f9bbbe3c15ae9ba4ed0801cbc512d81d
David J. Veer <david.veer@nfl.com>
2016-07-18 09:46:18 -0500

    Fixed a few syntax errors after converting to parameterized cmdlet

--- Do-StorageVMotion.ps1
+++ Do-StorageVMotion.ps1
@@ -1,13 +1,25 @@
-# Move VMs (Storage vMotion) between datastores in the nosnapshots_ds* datastores from where they exist currently
-$VMs = Get-VM (Get-Content C:\temp\storage_vmotions\dsc3-la3stor-ds0-ds2_all.txt )  
+# Move VMs (Storage vMotion) between datastores in the snapshots_ds* datastore cluster from where they exist currently
+[CmdletBinding()]
+Param(
+  # File name with list of VMs
+  [Parameter(Mandatory=$True)]
+  [string]$filename,
+  
+  # Number of VMs from that list to migrate
+  [Parameter(Mandatory=$True)]
+  [string]$number_of_lines
+    
+)
+
+$VMs = Get-VM (Get-Content $filename -First $number_of_lines)  
 foreach ($v in $VMs) {
-    If ((Get-VM $v | Get-Datastore) -notlike "snapshots_ds*")  
+If ((Get-VM $v | Get-Datastore) -notlike "snapshots_ds*")  
     {
         $random_new_datastore = Get-Datastore snapshots_ds* | Where-object {$_.freespacegb -GE ($v.provisionedspaceGB*1.25)} | Get-Random -Count 1  
         do { Move-VM -VM $v -Datastore $random_New_Datastore -Confirm:$false -Verbose } While ($_.freespacegb -gt 750)
     }
-    Else {
+Else {
         Write-Output "$v VM was already in one of the snapshots_ds* datastores, skipping."
-    }
+     }
         Write-Output " "
-    }
+}
